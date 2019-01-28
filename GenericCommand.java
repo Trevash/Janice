ea4c5ac7dd21ab140979c10be34fc33dc53f414a
CCranney <11773171+CCranney@users.noreply.github.com>
2019-01-28 13:39:08 -0700

    Add files via upload

---
+++ GenericCommand.java
@@ -0,0 +1,55 @@
+package GenericCommand;
+import java.util.*;
+import java.lang.reflect.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import StringProcessor.*;
+
+public class GenericCommand implements java.io.Serializable {
+    private String _className;
+    private String _methodName;
+    private Class<?>[] _paramTypes;
+    private Object[] _paramValues;
+    
+    
+     public GenericCommand(String className, String methodName,
+                          Class<?>[] paramTypes, Object[] paramValues) {
+        _className = className;
+        _methodName = methodName;
+        _paramTypes = paramTypes;
+        _paramValues = paramValues;
+    }
+    public String getMethod(){
+        return _methodName;
+    }
+    public Object[] getParamValues(){
+        return _paramValues;
+    }
+    private void readObject(ObjectInputStream input) 
+                            throws IOException, ClassNotFoundException {
+        input.defaultReadObject();
+    }
+    private void readObject(ObjectOutputStream output) 
+                            throws IOException, ClassNotFoundException {
+        output.defaultWriteObject();
+    }
+    
+    
+    
+    
+    
+    public Object execute() {
+
+        try {
+            Class<?> receiver = Class.forName(_className);
+            Method method = receiver.getMethod(_methodName, _paramTypes);
+            Object o = method.invoke(null, _paramValues);
+            return o;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return "null";
+        }
+    } 
+}
