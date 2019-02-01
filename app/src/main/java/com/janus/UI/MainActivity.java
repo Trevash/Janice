6b7644029cd70b9dd25cec4341c706bbed6ebd22
Trevor Garn <trevash77@gmail.com>
2019-01-31 21:08:13 -0700

    Forced landscape mode while in Activity

--- app/src/main/java/com/janus/UI/MainActivity.java
+++ app/src/main/java/com/janus/UI/MainActivity.java
@@ -1,5 +1,6 @@
 package com.janus.UI;
 
+import android.content.pm.ActivityInfo;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Iconify.with(new FontAwesomeModule());
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
