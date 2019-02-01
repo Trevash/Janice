ca939f3f2e9f2f7b77ce9c0ed389a7ae399028c6
jgilles3 <31894755+jgilles3@users.noreply.github.com>
2019-01-31 17:35:00 -0700

    Added Coy's and Caleb's files

---
+++ shared/src/main/java/com/bignerdranch/android/shared/Results.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.shared;
+
+/**
+ * Created by coy on 1/14/2019.
+ */
+
+public class Results {
+    public boolean success = false;
+    public Object data = null;
+    public String errorInfo = null;
+
+    public Results(boolean newSuccess, Object newData, String newErrorInfo){
+        success = newSuccess;
+        data = newData;
+        errorInfo = newErrorInfo;
+    }
+}
---
+++ shared/src/main/java/com/bignerdranch/android/shared/Serializer.java
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.shared;
+
+import com.google.gson.Gson;
+
+public class Serializer {
+    private static Serializer sr = new Serializer();
+    public static Serializer getInstance() {
+        if (sr == null){
+            sr = new Serializer();
+        }
+        return sr;
+    }
+    Gson parser = new Gson();
+
+    public String serializeObject(Object obj){
+        return parser.toJson(obj);
+    }
+
+    public Command deserializeObject(String str){ return parser.fromJson(str, Command.class); }
+
+    public String serializeResults(Results r){
+        return parser.toJson(r);
+    }
+
+    public Results deserializeResults(String str){
+        return parser.fromJson(str, Results.class);
+    }
+
+}
