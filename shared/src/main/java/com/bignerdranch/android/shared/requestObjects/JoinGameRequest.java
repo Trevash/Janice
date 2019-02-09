5959dace2f4a6cd43e65a1b43825a877b2298b89
Trevor Garn <trevash77@gmail.com>
2019-02-09 14:25:58 -0700

    Added authToken to join game request

--- shared/src/main/java/com/bignerdranch/android/shared/requestObjects/JoinGameRequest.java
+++ shared/src/main/java/com/bignerdranch/android/shared/requestObjects/JoinGameRequest.java
@@ -1,12 +1,15 @@
 package com.bignerdranch.android.shared.requestObjects;
 
 import com.bignerdranch.android.shared.models.gameModel;
+import com.bignerdranch.android.shared.models.authTokenModel;
 
 public class JoinGameRequest {
     private gameModel model;
+    private authTokenModel auth;
 
-    public JoinGameRequest(gameModel model) {
+    public JoinGameRequest(gameModel model, authTokenModel auth) {
         this.model = model;
+        this.auth = auth;
     }
 
     public gameModel getModel() {
@@ -15,5 +18,13 @@ public class JoinGameRequest {
 
     public void setModel(gameModel model) {
         this.model = model;
+    }
+
+    public authTokenModel getAuth() {
+        return auth;
+    }
+
+    public void setAuth(authTokenModel auth) {
+        this.auth = auth;
     }
 }
