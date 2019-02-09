e41c64c4a9d839576597b26095a731423e591f70
jgilles3 <31894755+jgilles3@users.noreply.github.com>
2019-02-09 14:06:00 -0700

    Changes to Lobby and playerModel

--- shared/src/main/java/com/bignerdranch/android/shared/models/playerModel.java
+++ shared/src/main/java/com/bignerdranch/android/shared/models/playerModel.java
@@ -3,16 +3,23 @@ package com.bignerdranch.android.shared.models;
 public class playerModel {
     private usernameModel userName = null;
     private playerIDModel id = null;
-    private boolean isReady = false;
 
-    public playerIDModel getId() {
-        return id;
-    }
-
-    public playerModel(usernameModel userName, playerIDModel id, boolean isReady) {
+    public playerModel(usernameModel userName, playerIDModel id, boolean isReady, boolean isHost) {
         this.userName = userName;
         this.id = id;
         this.isReady = isReady;
+        this.isHost = isHost;
+    }
+
+    public boolean isHost() {
+        return isHost;
+    }
+
+    private boolean isReady = false;
+    private boolean isHost = false;
+
+    public playerIDModel getId() {
+        return id;
     }
 
     public usernameModel getUserName() {
