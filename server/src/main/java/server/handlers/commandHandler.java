054d2a8ceae8d6c2509436e4f820d3cb4f05951b
CCranney <mellowcello13@gmail.com>
2019-02-11 17:52:15 -0700

    Results object contains serialized JSON string for data variable, getData returns object

--- server/src/main/java/server/handlers/commandHandler.java
+++ server/src/main/java/server/handlers/commandHandler.java
@@ -25,8 +25,8 @@ public class commandHandler extends handlerBase {
         Results results = command.execute();
         String ss = Serializer.getInstance().serializeObject(results);
 
-        if(results.isSuccess()){ return (String) results.getData(); }
-        else{ return (String) results.getData(); }
+        if(results.isSuccess()){ return (String) results.getJSONdata(); }
+        else{ return (String) results.getJSONdata(); }
     }
 
     public Results register(String username, String password) throws Exception{
@@ -38,7 +38,7 @@ public class commandHandler extends handlerBase {
 
         AuthData data = new AuthData(auth);
 
-        return new Results("Register", true, data);
+        return new Results("Register", true, Serializer.getInstance().serializeObject(data));
     }
 
     public Results login(String username, String password) throws Exception {
@@ -52,7 +52,7 @@ public class commandHandler extends handlerBase {
         curUser.setAuthToken(auth);
 
         AuthData data = new AuthData(auth);
-        return new Results("Login", true, data);
+        return new Results("Login", true, Serializer.getInstance().serializeObject(data));
     }
 
     public Results createGame(CreateGameRequest request) throws Exception {
@@ -71,7 +71,7 @@ public class commandHandler extends handlerBase {
 
         serverModel.getInstance().joinGame(request);
 
-        return new Results("Join", true, new GameListData());
+        return new Results("Join", true, Serializer.getInstance().serializeObject(new GameListData()));
     }
 
     public Results startGame(StartGameRequest request) throws Exception {
@@ -81,6 +81,6 @@ public class commandHandler extends handlerBase {
 
         serverModel.getInstance().startGame(request);
 
-        return new Results("Start", true, new GameListData());
+        return new Results("Start", true, Serializer.getInstance().serializeObject(new GameListData()));
     }
 }
--- shared/src/main/java/com/bignerdranch/android/shared/resultobjects/Results.java
+++ shared/src/main/java/com/bignerdranch/android/shared/resultobjects/Results.java
@@ -1,14 +1,17 @@
 package com.bignerdranch.android.shared.resultobjects;
 
+import com.bignerdranch.android.shared.Serializer;
+import com.google.gson.Gson;
+
 public class Results {
     private String type;
     private boolean success;
-    private Object data;
+    private String JSONdata;
 
-    public Results(String type, boolean success, Object data) {
+    public Results(String type, boolean success, String JSONdata) {
         this.type = type;
         this.success = success;
-        this.data = data;
+        this.JSONdata = JSONdata;
     }
 
     public String getType() {
@@ -19,12 +22,18 @@ public class Results {
         return success;
     }
 
+    public String getJSONdata() {
+    	return JSONdata;
+    }
+    
     //Object should be the data object specific to the type
     public Object getData() {
-        return data;
+    	Gson gson = new Gson();
+    	Object dataObject = gson.fromJson(JSONdata, Object.class); //possibly return classType not Object.class
+        return dataObject;
     }
 
-    public void setData(Object data) {
-        this.data = data;
+    public void setData(String JSONdata) {
+        this.JSONdata = JSONdata;
     }
 }
