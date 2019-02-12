92512cb4bedac3261e0693d2340e5802047f9b79
Trevor Garn <trevash77@gmail.com>
2019-02-11 17:34:27 -0700

    UI update from change to client model

--- app/src/main/java/com/janus/ClientModel.java
+++ app/src/main/java/com/janus/ClientModel.java
@@ -9,11 +9,17 @@ import com.bignerdranch.android.shared.resultobjects.GameListData;
 import com.janus.Communication.TtRClient;
 
 public class ClientModel {
+
+    public interface CurrentView {
+        void updateUI();
+    }
+
     private gameModel game;
     private List<gameModel> serverGameList;
     private userModel user;
     private authTokenModel auth;
     private static ClientModel model;
+    private CurrentView currentView;
 
     private ClientModel() {}
 
@@ -22,6 +28,10 @@ public class ClientModel {
             model = new ClientModel();
         }
         return model;
+    }
+
+    public void setCurrentView(CurrentView view) {
+        this.currentView = view;
     }
 
     public gameModel getGame() {
