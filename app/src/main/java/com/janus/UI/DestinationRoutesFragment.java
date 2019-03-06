cf37bc3511a6e65167bb5f5cd16251b64c40d6a2
Trevor Garn <trevash77@gmail.com>
2019-03-05 17:31:29 -0700

    Included Status fragment with viewpager for the chat, destination, and history fragment

---
+++ app/src/main/java/com/janus/UI/DestinationRoutesFragment.java
@@ -0,0 +1,26 @@
+package com.janus.UI;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.janus.R;
+
+public class DestinationRoutesFragment extends Fragment {
+
+    public DestinationRoutesFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_destination_routes, container, false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/janus/UI/StatusFragment.java
@@ -0,0 +1,81 @@
+package com.janus.UI;
+
+import android.support.design.widget.TabLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.janus.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusFragment extends Fragment {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    public StatusFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_status, container, false);
+
+        viewPager = v.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = v.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return v;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new ChatFragment(), "Chat");
+        adapter.addFragment(new DestinationRoutesFragment(), "Destinations");
+        adapter.addFragment(new GameHistoryFragment(), "History");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
