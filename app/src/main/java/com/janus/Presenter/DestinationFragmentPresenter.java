cb4ea09090757dda1f8feb84a4a8a62157a954f0
jgilles3 <31894755+jgilles3@users.noreply.github.com>
2019-03-09 15:36:28 -0700

    Most of the Functionality for DestinationSelectFragment

--- app/src/main/java/com/janus/Presenter/DestinationFragmentPresenter.java
+++ app/src/main/java/com/janus/Presenter/DestinationFragmentPresenter.java
@@ -1,4 +1,35 @@
 package com.janus.Presenter;
 
-public class DestinationFragmentPresenter {
+
+import com.bignerdranch.android.shared.models.DestinationCardModel;
+import com.janus.ClientFacade;
+import com.janus.ClientModel;
+
+import java.util.List;
+
+public class DestinationFragmentPresenter implements ClientFacade.Presenter {
+    public interface View {
+        void updateDestinationCards(/*List<DestinationCardModel> cards*/);
+    }
+    private DestinationFragmentPresenter.View view;
+    private ClientFacade facade = ClientFacade.getInstance();
+    private ClientModel model = ClientModel.getInstance();
+
+    public DestinationFragmentPresenter(View view) {
+        this.view = view;
+    }
+
+    public void updateUI(){
+        view.updateDestinationCards(/*model.getGame().getDestinationCards()*/);
+        //ToDo: Need a way of getting destination cards
+    }
+
+    public void setFragment() {
+        facade.setPresenter(this);
+    }
+
+    public void selectDestinationCards(int[] indices){
+        //Start task to get destination cards
+        //Request objects for getting destination cards
+    }
 }
--- app/src/main/java/com/janus/UI/DestinationSelectFragment.java
+++ app/src/main/java/com/janus/UI/DestinationSelectFragment.java
@@ -1,18 +1,42 @@
 package com.janus.UI;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.bignerdranch.android.shared.models.DestinationCardModel;
+import com.janus.Presenter.DestinationFragmentPresenter;
 import com.janus.R;
 
-public class DestinationSelectFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DestinationSelectFragment extends Fragment implements DestinationFragmentPresenter.View {
 
     public interface Context {
         void onFinishAction();
     }
+
+    //ToDo: Disable if it's not your turn, connect to server to get destination cards.
+    //ToDo: Also identify whether this is your first time drawing destination cards
+
+    private DestinationFragmentPresenter presenter;
+    private Context mContext;
+    private TextView mPrompt;
+    private TextView mDestination1;
+    private TextView mDestination2;
+    private TextView mDestination3;
+    private List<DestinationCardModel> availableDestinationCards = new ArrayList<>();
+    private List<DestinationCardModel> selectedDestinationCards = new ArrayList<>();
+    private boolean destination1Selected = false;
+    private boolean destination2Selected = false;
+    private boolean destination3Selected = false;
+    private Button mAcceptButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +46,95 @@ public class DestinationSelectFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_destination_select, container, false);
+        View v = inflater.inflate(R.layout.fragment_destination_select, container, false);
+        presenter = new DestinationFragmentPresenter(this);
+        presenter.setFragment();
+        mContext = (Context) getActivity();
+
+        mPrompt = v.findViewById(R.id.destinationSelectPrompt);
+
+        mDestination1 = v.findViewById(R.id.destination1);
+        mDestination1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!destination1Selected){
+                    destination1Selected = true;
+                    selectedDestinationCards.add(availableDestinationCards.get(0));
+                    mDestination1.setTextColor(Color.GREEN);
+                    updateButton();
+                } else {
+                    destination1Selected = false;
+                    selectedDestinationCards.remove(availableDestinationCards.get(0));
+                    mDestination1.setTextColor(Color.BLACK);
+                    updateButton();
+                }
+            }
+        });
+
+        mDestination2 = v.findViewById(R.id.destination2);
+        mDestination2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!destination2Selected){
+                    destination2Selected = true;
+                    selectedDestinationCards.add(availableDestinationCards.get(1));
+                    mDestination2.setTextColor(Color.GREEN);
+                    updateButton();
+                } else {
+                    destination2Selected = false;
+                    selectedDestinationCards.remove(availableDestinationCards.get(1));
+                    mDestination2.setTextColor(Color.BLACK);
+                    updateButton();
+                }
+            }
+        });
+
+        mDestination3 = v.findViewById(R.id.destination3);
+        mDestination3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!destination3Selected){
+                    destination3Selected = true;
+                    selectedDestinationCards.add(availableDestinationCards.get(2));
+                    mDestination3.setTextColor(Color.GREEN);
+                    updateButton();
+                } else {
+                    destination3Selected = false;
+                    selectedDestinationCards.remove(availableDestinationCards.get(2));
+                    mDestination3.setTextColor(Color.BLACK);
+                    updateButton();
+                }
+            }
+        });
+
+        mAcceptButton = v.findViewById(R.id.acceptDestinations);
+        mAcceptButton.setEnabled(false);
+        mAcceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //presenter.selectDestinationCards(selectedDestinationCards);
+                mContext.onFinishAction();
+            }
+        });
+
+        presenter.updateUI();
+        return v;
+    }
+
+    @Override
+    public void updateDestinationCards(/*destinationCards cards*/) {
+        //availableDestinationCards = cards;
+        //mPrompt.setText();
+        //mDestination1.setText(card.at(0).getCity1() + " - " + card.at(0).getCity2() + ": " + card.at(0).getLength();
+        //mDestination2.setText(card.at(1).getCity1() + " - " + card.at(1).getCity2() + ": " + card.at(1).getLength();
+        //mDestination3.setText(card.at(2).getCity1() + " - " + card.at(2).getCity2() + ": " + card.at(2).getLength();
+    }
+
+    public void updateButton(){
+        if(selectedDestinationCards.size() >= 2){
+            mAcceptButton.setEnabled(true);
+        } else {
+            mAcceptButton.setEnabled(false);
+        }
     }
 }
