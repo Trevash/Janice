ade916ef03d9849c12134902acda72de03617ea2
jgilles3 <31894755+jgilles3@users.noreply.github.com>
2019-04-05 14:59:14 -0600

    Fixed commandHandler bug

--- shared/src/main/java/com/bignerdranch/android/shared/Constants.java
+++ shared/src/main/java/com/bignerdranch/android/shared/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String IP_ADDRESS =
             //"192.168.1.100"; // Jason's home IP address
             //"192.168.254.216";
-            "192.168.254.186";
+            "192.168.254.249";
             //"10.37.93.67"; // Regular IP address for group testing
             //"10.34.241.184"; // another IP address for group testing
     public static final int PORT = 8087;
